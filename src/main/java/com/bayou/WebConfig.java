704de28b98b56b9315c8bef90f0e9fc2ac051f7a
sev <jleaton@uno.edu>
2017-02-08 15:21:49 -0600

    Adds CORS functionality via web config

---
+++ src/main/java/com/bayou/WebConfig.java
@@ -0,0 +1,24 @@
+package com.bayou;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by sev on 2/8/17.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("PUT", "DELETE","GET","POST")
+                .allowedHeaders("header1", "header2", "header3")
+                .exposedHeaders("header1", "header2")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
