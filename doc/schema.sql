a1d78ad0b210911ac0239d7c8fce5fe13a654957
rnguillory <rachel.n.guillory@gmail.com>
2017-02-10 17:56:48 -0600

    Updated schema to include password hash and salt fields.  Also, added the UNIQUE keyword to a few fields, such as account_name and email, as we should not have two accounts for the same person, and there should be only one account with a given account_name.

--- doc/schema.sql
+++ doc/schema.sql
@@ -1,13 +1,23 @@
 CREATE TABLE users (
 	user_id serial PRIMARY KEY,
-	account_name VARCHAR(30) NOT NULL,
+	account_name VARCHAR(30) UNIQUE NOT NULL,
+	password_hash CHARACTER(128) NOT NULL,
+	password_salt CHARACTER(64) NOT NULL,
 	first_name VARCHAR(20),
 	last_name VARCHAR(20),
-	email VARCHAR(120) NOT NULL,
+	email VARCHAR(120) UNIQUE NOT NULL,
 	phone_number VARCHAR(12),
 	facebookID VARCHAR(120),
 	twitterHandle VARCHAR(100)
 );
+
+CREATE TABLE unverified_users (
+    unverified_user_id serial PRIMARY KEY,
+    email VARCHAR(120) UNIQUE NOT NULL,
+    password_hash CHARACTER(128) NOT NULL,
+    password_salt CHARACTER(64) NOT NULL,
+    verification_code integer NOT NULL
+)
 
 CREATE TABLE ads (
 	ad_id serial PRIMARY KEY,
@@ -22,7 +32,7 @@ CREATE TABLE ads (
 
 CREATE TABLE categories (
 	category_id serial PRIMARY KEY,
-	name VARCHAR(255),
+	name VARCHAR(255) NOT NULL,
 	description text,
 	parent_category_id integer REFERENCES categories(category_id),
 );
