20a4ae0a80449520c62fb1e86b6aeaee7eebb353
Joshua <joshua.eaton@ge.com>
2017-02-16 16:35:05 -0600

    Updates typename to typname

--- src/main/resources/db/migration/V0001__initial_tables.sql
+++ src/main/resources/db/migration/V0001__initial_tables.sql
@@ -1,6 +1,6 @@
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typename = 'ad_type') THEN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'ad_type') THEN
         CREATE TYPE ad_type AS ENUM ('offer', 'seek');
     END IF;
 END$$;
