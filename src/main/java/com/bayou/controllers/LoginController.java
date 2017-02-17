2105f8e972ec914b2b613f5f9c0675f79be4e66d
Joshua <joshua.eaton@ge.com>
2017-02-17 13:40:07 -0600

    Updates endpoint to accept a LoginView instead of a UserView

--- src/main/java/com/bayou/controllers/LoginController.java
+++ src/main/java/com/bayou/controllers/LoginController.java
@@ -1,6 +1,10 @@
 package com.bayou.controllers;
 
+import com.bayou.converters.LoginConverter;
+import com.bayou.converters.LoginViewConverter;
+import com.bayou.converters.UserConverter;
 import com.bayou.managers.impl.UserManager;
+import com.bayou.views.impl.LoginView;
 import com.bayou.views.impl.UserView;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +16,30 @@ import org.springframework.web.bind.annotation.*;
  * Created by joshuaeaton on 1/31/17.
  */
 @RestController
-@RequestMapping("service/v1")
+@RequestMapping("service/v1/login")
 public class LoginController {
+
     @Autowired
     UserManager userManager = new UserManager();
+    @Autowired
+    LoginConverter converter = new LoginConverter();
 
     @ApiOperation(value = "Login as user by email or account name", response = ResponseEntity.class)
-    @RequestMapping(value = "/login", method = RequestMethod.POST)   //sets the mapping url and the HTTP method
-    public ResponseEntity<UserView> login(@RequestBody UserView userView) {
+    @RequestMapping(value = "/{string}", method = RequestMethod.POST)   //sets the mapping url and the HTTP method
+    public ResponseEntity<LoginView> login(@RequestBody LoginView loginView) {
 
-        return new ResponseEntity<>(userManager.login(userView), HttpStatus.OK);
+
+        if(!loginView.getEmail().equals(null)) { //if email field is not null, get the user by email
+            UserView returnedUser = userManager.getByEmail(loginView.getEmail());   //stores the returned object into returnedUser
+            loginView = converter.convertToLoginView(returnedUser); //converts the returned user to a login view object
+        }
+        else if (!loginView.getAccount_name().equals(null)) { //if account name is not null, get the user by account name
+            //UserView returnedUer = userManager.getByAccountName(loginVIew.getByAccountName);
+            //loginView = converter.convertToLoginView(returnedUser); //converst the returned user to a login view object
+        } else {
+            //throw exception here
+        }
+
+        return new ResponseEntity<>(loginView, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/bayou/views/impl/LoginView.java
@@ -0,0 +1,45 @@
+package com.bayou.views.impl;
+
+/**
+ * Created by joshuaeaton on 2/17/17.
+ */
+public class LoginView {
+
+    private String email;
+    private String account_name;
+    private String passwordSalt;
+    private String passwordHash;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAccount_name() {
+        return account_name;
+    }
+
+    public void setAccount_name(String account_name) {
+        this.account_name = account_name;
+    }
+
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    public void setPasswordSalt(String passwordSalt) {
+        this.passwordSalt = passwordSalt;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+}
