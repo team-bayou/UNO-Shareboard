ac835a520d766943512549b5c69737e99881e718
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2017-02-17 11:56:40 -0600

    Add LoginController, Remove unnecessary stuff from UserControler

--- src/main/java/com/bayou/controllers/UserController.java
+++ src/main/java/com/bayou/controllers/UserController.java
@@ -24,20 +24,6 @@ public class UserController {
         return new ResponseEntity<>(userManager.getById(id), HttpStatus.OK);
     }
 
-/*    @ApiOperation(value= "Get a user by account_name" , response = ResponseEntity.class)
-    @RequestMapping(value = "/{account_name}", method = RequestMethod.GET)   //sets the mapping url and the HTTP method
-    public ResponseEntity<UserView> getByAccountName(@PathVariable String accountName) {
-
-        return new ResponseEntity<>( userManager.getByAccountName(accountName), HttpStatus.OK);
-    }*/
-
-    @ApiOperation(value = "Get a user by email", response = ResponseEntity.class)
-    @RequestMapping(value = "/email/{email}", method = RequestMethod.GET)   //sets the mapping url and the HTTP method
-    public ResponseEntity<UserView> getByEmail(@PathVariable("email") String email) {
-
-        return new ResponseEntity<>(userManager.getByEmail(email), HttpStatus.OK);
-    }
-
     @ApiOperation(value = "Add a user", response = ResponseEntity.class)
     @RequestMapping(value = "/add", method = RequestMethod.POST)   //sets the mapping url and the HTTP method
     public ResponseEntity<UserView> add(@RequestBody UserView userView) {
