f0ca330d4e1103f055a96d2dd1798ef32d925975
Joshua <joshua.eaton@ge.com>
2017-02-17 20:16:03 -0600

    Adds exception handling for attempting to delete a non-existent user

--- src/main/java/com/bayou/controllers/UserController.java
+++ src/main/java/com/bayou/controllers/UserController.java
@@ -3,7 +3,9 @@ package com.bayou.controllers;
 import com.bayou.managers.impl.UserManager;
 import com.bayou.views.impl.UserView;
 import io.swagger.annotations.ApiOperation;
+import org.postgresql.util.PSQLException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,14 +30,21 @@ public class UserController {
     @RequestMapping(value = "/add", method = RequestMethod.POST)   //sets the mapping url and the HTTP method
     public ResponseEntity<UserView> add(@RequestBody UserView userView) {
 
-        return new ResponseEntity<>(userManager.add(userView), HttpStatus.OK);
+
+           ResponseEntity<UserView> responseEntity = new ResponseEntity<UserView>(userManager.add(userView), HttpStatus.OK);
+
+        return responseEntity;
     }
 
     @ApiOperation(value = "Delete a user", response = ResponseEntity.class)
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)   //sets the mapping url and the HTTP method
     public ResponseEntity delete(@PathVariable("id") Long id) {
-
-            userManager.delete(id);
+            try {
+                userManager.delete(id);
+            }
+            catch (EmptyResultDataAccessException e) {
+                System.out.println("The user with ID:" + id + " does not exist in the database ");
+            }
             return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
