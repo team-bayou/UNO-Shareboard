fdc9c73d83e3f60e6ffda27c03b28b93fc7d79f1
Rachel Guillory <rachel.n.guillory@gmail.com>
2017-02-17 19:24:40 -0600

    Unverified user update, removing account_name, add verification_code.

--- src/main/resources/db/migration/V0002__add_user_type.sql
+++
@@ -1,6 +0,0 @@
-DROP TYPE IF EXISTS user_type;
-IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_type') THEN
-   CREATE TYPE user_type AS ENUM ('admin', 'standard');
-END IF;
-
-ALTER TABLE users ADD COLUMN user_type user_type NOT NULL DEFAULT 'standard';
