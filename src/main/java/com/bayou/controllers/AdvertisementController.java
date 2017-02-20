3588cbdd4efa344245e231b3e4a859d7580ca77d
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2017-02-20 12:38:22 -0600

    Update AdController, Add AdConverter

--- src/main/java/com/bayou/controllers/AdvertisementController.java
+++ src/main/java/com/bayou/controllers/AdvertisementController.java
@@ -1,31 +1,34 @@
 package com.bayou.controllers;
 
-import com.bayou.managers.impl.UserManager;
-import com.bayou.views.impl.UserView;
+import com.bayou.managers.impl.AdvertisementManager;
+import com.bayou.views.impl.AdvertisementView;
 import io.swagger.annotations.ApiOperation;
 import javassist.NotFoundException;
-import org.postgresql.util.PSQLException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
- * Created by joshuaeaton on 1/31/17.
+ * File: AdvertisementController
+ * Package: com.bayou.controllers
+ * Author: Stefan Haselwanter
+ * Created on: 2/20/17
  */
 @RestController
-@RequestMapping("service/v1/users")
-public class UserController {
+@RequestMapping("service/v1/advertisements")
+public class AdvertisementController {
     @Autowired
-    UserManager userManager = new UserManager();
+    AdvertisementManager manager = new AdvertisementManager();
 
-    @ApiOperation(value = "Get a user by id", response = ResponseEntity.class)
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)   //sets the mapping url and the HTTP method
-    public ResponseEntity<UserView> getById(@PathVariable("id") Long id) throws NotFoundException {
-
-        ResponseEntity<UserView> responseEntity = null;
-        try {responseEntity = new ResponseEntity<>(userManager.get(id), HttpStatus.OK);
+    @ApiOperation(value = "Get a list of advertisements", response = ResponseEntity.class)
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<List<AdvertisementView>> getAll() throws NotFoundException {
+        ResponseEntity<List<AdvertisementView>> responseEntity;
+        try {
+            responseEntity = new ResponseEntity<>(manager.getAll(), HttpStatus.OK);
         } catch (NotFoundException e) {
             responseEntity = new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
@@ -33,12 +36,12 @@ public class UserController {
         return responseEntity;
     }
 
-    @ApiOperation(value = "Get a user by account name", response = ResponseEntity.class)
-    @RequestMapping(value = "/accountName/{accountName}", method = RequestMethod.GET)   //sets the mapping url and the HTTP method
-    public ResponseEntity<UserView> getByAccountName(@PathVariable("accountName") String accountName) throws NotFoundException {
-
-        ResponseEntity<UserView> responseEntity = null;
-        try {responseEntity = new ResponseEntity<>(userManager.getByAccountName(accountName), HttpStatus.OK);
+    @ApiOperation(value = "Get an advertisement by id", response = ResponseEntity.class)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<AdvertisementView> getById(@PathVariable("id") Long id) throws NotFoundException {
+        ResponseEntity<AdvertisementView> responseEntity = null;
+        try {
+            responseEntity = new ResponseEntity<>(manager.get(id), HttpStatus.OK);
         } catch (NotFoundException e) {
             responseEntity = new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
@@ -46,31 +49,16 @@ public class UserController {
         return responseEntity;
     }
 
-    @ApiOperation(value = "Get a user by email", response = ResponseEntity.class)
-    @RequestMapping(value = "/email/{email}", method = RequestMethod.GET)   //sets the mapping url and the HTTP method
-    public ResponseEntity<UserView> getByEmail(@PathVariable("email") String email) throws NotFoundException {
-
-        ResponseEntity<UserView> responseEntity = null;
-        try {responseEntity = new ResponseEntity<>(userManager.getByEmail(email), HttpStatus.OK);
-        } catch (NotFoundException e) {
-            responseEntity = new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-
-        return responseEntity;
+    @ApiOperation(value = "Add an advertisement", response = ResponseEntity.class)
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ResponseEntity add(@RequestBody AdvertisementView advertisementView) {
+        return new ResponseEntity(manager.add(advertisementView));
     }
 
-    @ApiOperation(value = "Add a user", response = ResponseEntity.class)
-    @RequestMapping(value = "/add", method = RequestMethod.POST)   //sets the mapping url and the HTTP method
-    public ResponseEntity add(@RequestBody UserView userView) {
-
-        return new ResponseEntity(userManager.add(userView));
-    }
-
-    @ApiOperation(value = "Delete a user", response = ResponseEntity.class)
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)   //sets the mapping url and the HTTP method
+    @ApiOperation(value = "Delete an advertisement", response = ResponseEntity.class)
+    @RequestMapping(value = "/{id}/delete", method = RequestMethod.DELETE)
     public ResponseEntity delete(@PathVariable("id") Long id) {
-
-        userManager.delete(id);
+        manager.delete(id);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
--- src/main/java/com/bayou/converters/AdvertisementConverter.java
+++ src/main/java/com/bayou/converters/AdvertisementConverter.java
@@ -1,49 +1,51 @@
 package com.bayou.converters;
 
-import com.bayou.domains.User;
-import com.bayou.views.impl.UserView;
+import com.bayou.domains.Advertisement;
+import com.bayou.views.impl.AdvertisementView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * Created by joshuaeaton on 1/31/17.
+ * File: AdvertisementConverter
+ * Package: com.bayou.converters
+ * Author: Stefan Haselwanter
+ * Created on: 2/20/17
  */
-@Component("UserConverter") //registers this class as a component bean so that it can be pulled into the application context
-public class UserConverter {
+@Component("AdvertisementConverter")
+public class AdvertisementConverter {
+    @Autowired
+    UserConverter userConverter = new UserConverter();
 
+    public AdvertisementView convertToView(Advertisement domain) {
+        AdvertisementView view = new AdvertisementView();
+        view.setId(domain.getId());
+        view.setTitle(domain.getTitle());
+        view.setDescription(domain.getDescription());
+        view.setCategoryId(domain.getCategoryId());
+        view.setOwner(userConverter.convertToView(domain.getOwner()));
+        view.setTimePublished(domain.getTimePublished());
+        view.setExpirationDate(domain.getExpirationDate());
+        view.setAdType(domain.getAdType());
+        view.setPrice(domain.getPrice());
+        view.setTradeItem(domain.getTradeItem());
 
-    public UserView convertToView(User user) {
-        UserView view = new UserView(); //this will be the newly created View version of the domain Object
-        view.setId(user.getId());
-        view.setAccountName(user.getAccountName());
-        view.setFirstName(user.getFirstName());
-        view.setLastName(user.getLastName());
-        view.setPasswordHash(user.getPasswordHash());
-        view.setPasswordSalt(user.getPasswordSalt());
-        view.setUserType(user.getUserType());
-        view.setEmail(user.getEmail());
-        view.setPhoneNumber(user.getPhoneNumber());
-        view.setFacebookId(user.getFacebookId());
-        view.setTwitterHandle(user.getTwitterHandle());
-        view.setImageId(user.getImageId());
-
-        return view;    //return the View version of the given domain Object
+        return view;
     }
 
-    public User convertToDomain(UserView userView) {
-        User domainUser = new User();   //this will be the newly created Domain version of the view Object
-        domainUser.setAccountName(userView.getAccountName());
-        domainUser.setFirstName(userView.getFirstName());
-        domainUser.setLastName(userView.getLastName());
-        domainUser.setPasswordHash(userView.getPasswordHash());
-        domainUser.setPasswordSalt(userView.getPasswordSalt());
-        domainUser.setUserType(userView.getUserType());
-        domainUser.setEmail(userView.getEmail());
-        domainUser.setPhoneNumber(userView.getPhoneNumber());
-        domainUser.setFacebookId(userView.getFacebookId());
-        domainUser.setTwitterHandle(userView.getTwitterHandle());
-        domainUser.setImageId(userView.getImageId());
+    public Advertisement convertToDomain(AdvertisementView view) {
+        Advertisement domain = new Advertisement();
+        domain.setId(view.getId());
+        domain.setTitle(view.getTitle());
+        domain.setDescription(view.getDescription());
+        domain.setCategoryId(view.getCategoryId());
+        domain.setOwner(userConverter.convertToDomain(view.getOwner()));
+        domain.setTimePublished(view.getTimePublished());
+        domain.setExpirationDate(view.getExpirationDate());
+        domain.setAdType(view.getAdType());
+        domain.setPrice(view.getPrice());
+        domain.setTradeItem(view.getTradeItem());
 
-        return domainUser;  //return the Domain version of the given view Object
+        return domain;
     }
 }
 
