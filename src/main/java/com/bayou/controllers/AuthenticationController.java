3e6434e7ca4c4be990cd62008f947a1825dfc961
rnguillory <rachel.n.guillory@gmail.com>
2017-02-21 15:51:11 -0600

    Authentication(Login/Verification) update.

--- src/main/java/com/bayou/controllers/AuthenticationController.java
+++ src/main/java/com/bayou/controllers/AuthenticationController.java
@@ -2,11 +2,14 @@ package com.bayou.controllers;
 
 import com.bayou.engines.AuthenticationEngine;
 import com.bayou.exceptions.VerificationException;
+import com.bayou.managers.impl.UnverifiedUserManager;
+import com.bayou.managers.impl.UserManager;
 import com.bayou.views.impl.LoginView;
 import com.bayou.views.impl.UserView;
 import com.bayou.views.impl.VerifyUserView;
 import io.swagger.annotations.ApiOperation;
 import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("service/v1/auth")
 public class AuthenticationController {
-
+    @Autowired
     AuthenticationEngine authenticationEngine = new AuthenticationEngine();
 
     @ApiOperation(value = "Login as user by email or account name", response = ResponseEntity.class)
