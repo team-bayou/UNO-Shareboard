8de20b2854195eff5e2d219d802f81a9ec899c5c
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2017-02-21 01:54:21 -0600

    Create BaseEntityView and update view classes

--- src/main/java/com/bayou/views/impl/BaseEntityView.java
+++ src/main/java/com/bayou/views/impl/BaseEntityView.java
@@ -1,10 +1,38 @@
 package com.bayou.views.impl;
 
+import com.bayou.views.IView;
+
 /**
  * File: BaseEntityView
  * Package: com.bayou.views.impl
  * Author: Stefan Haselwanter
  * Created on: 2/20/17
  */
-public class BaseEntityView {
+public abstract class BaseEntityView implements IView {
+    private Long id;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BaseEntityView that = (BaseEntityView) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
