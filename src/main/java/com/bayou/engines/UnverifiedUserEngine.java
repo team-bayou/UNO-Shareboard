8220b5e9cb6fdcb28d1fdf74c3fc8b0b7221fab6
sev <jleaton@uno.edu>
2017-02-24 13:25:06 -0600

    Added v3 sendgrid imp and updated imports

--- src/main/java/com/bayou/engines/UnverifiedUserEngine.java
+++ src/main/java/com/bayou/engines/UnverifiedUserEngine.java
@@ -1,7 +1,9 @@
 package com.bayou.engines;
-import com.sen
+
+import com.sendgrid.*;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 
 
 /**
@@ -11,17 +13,27 @@ import org.springframework.stereotype.Component;
 public class UnverifiedUserEngine {
 
 
-    public void sendVerificationCode(String verCode) {
-        SendGrid sendgrid = new SendGrid("SENDGRID_APIKEY");
+    public void sendVerificationCode(String verCode) throws IOException {
+        Email from = new Email("test@example.com");
+        String subject = "Sending with SendGrid is Fun";
+        Email to = new Email("test@example.com");
+        Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+        Mail mail = new Mail(from, subject, to, content);
 
-        SendGrid.Email email = new SendGrid.Email();
-
-        email.addTo("test@sendgrid.com");
-        email.setFrom("you@youremail.com");
-        email.setSubject("Sending with SendGrid is Fun");
-        email.setHtml("and easy to do anywhere, even with Java");
-
-        SendGrid.Response response = sendgrid.send(email);
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+        try {
+            request.method = Method.POST;
+            request.endpoint = "mail/send";
+            request.body = mail.build();
+            Response response = sg.api(request);
+            System.out.println(response.statusCode);
+            System.out.println(response.body);
+            System.out.println(response.headers);
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
     }
 
 }
