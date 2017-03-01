a73b735d3fabb6f2c46530acec9af14ae8061c3c
Joshua Eaton <jleaton.dev@gmail.com>
2017-03-01 13:54:01 -0600

    Enhancement/#38/send confirmation code (#64)
    * Adds sendgrid base arch
    
    * Added v3 sendgrid imp and updated imports
    
    * Updates request
    
    * Adds method call in unv add to send a verification email
    
    * Adds sendgrid base arch
    
    * Added v3 sendgrid imp and updated imports
    
    * Updates request
    
    * Adds method call in unv add to send a verification email
    
    * Adds support for Versioning in entities now. Also adds update user functionality
    
    * Adds Update User to postman collection
    
    * Adds automated verification code emailing triggered on adding of unv user
    
    * Added url for ver page in email
    
    * Updates url dynamically
    
    * Uncommented isNew in User class, made the User class implement Persistable<Long>.
    
    * Handles missing id case
    
    * Adds id field to update User request postman request
    
    * Delets hibernate file in postman
    
    * Added ID to the user update postman collection.
    
    * Adds sendgrid base arch
    
    * Added v3 sendgrid imp and updated imports
    
    * Updates request
    
    * Adds method call in unv add to send a verification email
    
    * Adds sendgrid base arch
    
    * Added v3 sendgrid imp and updated imports
    
    * Adds automated verification code emailing triggered on adding of unv user
    
    * Added url for ver page in email
    
    * Updates url dynamically

---
+++ src/main/java/com/bayou/engines/UnverifiedUserEngine.java
@@ -0,0 +1,37 @@
+package com.bayou.engines;
+
+import com.sendgrid.*;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+
+/**
+ * Created by joshuaeaton on 2/23/17.
+ */
+@Component
+public class UnverifiedUserEngine {
+
+
+    public void sendVerificationCode(String verCode , String recipient ) throws IOException {
+        Email from = new Email("unoshareboard@uno.edu");   //set who the email is from
+        String subject = "UNO Shareboard Verification Code"; //set the subject of the email
+        Email to = new Email(recipient); //set who the email is to be sent to
+        Content content = new Content("text/plain", "Verification Code: " + verCode + "\nVerification Link: "
+                + System.getenv("HEROKU_ENV") + "/verify?email=" + recipient);   //set the content of the email
+        Mail mail = new Mail(from, subject, to, content); //initialize a mail Object
+
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY")); //gets the api key for auth from env var
+        Request request = new Request();
+        try {
+            request.method = Method.POST; //specifies the HTTP Verb
+            request.endpoint = "mail/send"; //hits the endpoint on Sendgrid
+            request.body = mail.build();    //builds the request body
+            Response response = sg.api(request); //makes the request
+            System.out.println(response.statusCode); //print out status code
+            System.out.println(response.body);  //print out body
+            System.out.println(response.headers); //print out headers
+        } catch (IOException ex) {  //handles IO exceptions
+            throw ex; //throw an exception if there was a IO exception generated
+        }
+    }
+}
