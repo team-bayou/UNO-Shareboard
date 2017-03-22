d4fdeb3560bbd6b33d4ecd4e414338cfe690882f
Joshua <joshua.eaton@ge.com>
2017-03-22 15:33:04 -0500

    Adds http headers for basic auth to user entity

--- src/test/java/com/bayou/controllers/UserControllerTests.java
+++ src/test/java/com/bayou/controllers/UserControllerTests.java
@@ -3,6 +3,7 @@ package com.bayou.controllers;
 import com.bayou.utils.Mocks;
 import com.bayou.utils.Server;
 import com.bayou.views.UserView;
+import org.apache.commons.codec.binary.Base64;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,10 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
@@ -32,16 +30,28 @@ public class UserControllerTests {
 
     @Autowired
     private TestRestTemplate rest;
-
+    private HttpHeaders headers;
     private UserView view;
 
     @Before
     public void setup() {
+
+        /*Encode the auth credentials*/
+        String plainCreds = System.getenv("USERNAME")+":"+System.getenv("PASSWORD");
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
+
+        /*Construct HTTP headers*/
+        headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Creds);
+
+
         // Create category view and add category to db.
         rest.withBasicAuth(System.getenv("USERNAME"),System.getenv("PASSWORD"));
         view = Mocks.createUserView();
         ResponseEntity<Long> entity = rest.postForEntity(
-                Server.url() + RESOURCE_URL + "/add", new HttpEntity<>(view, Server.createHeadersJson()), Long.class);
+                Server.url() + RESOURCE_URL + "/add", new HttpEntity<>(view, Server.createHeadersJson()), Long.class, headers);
         view.setId(entity.getBody());
     }
 
@@ -55,7 +65,7 @@ public class UserControllerTests {
     public void testGetUserById() {
         // Get user by id.
         ResponseEntity<UserView> responseEntity = rest.getForEntity(
-                Server.url() + RESOURCE_URL + "/" + view.getId(), UserView.class);
+                Server.url() + RESOURCE_URL + "/" + view.getId(), UserView.class, headers);
 
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertTrue(responseEntity.getBody() != null);
@@ -65,7 +75,7 @@ public class UserControllerTests {
     public void testGetUserByAccountName() {
         // Get user by account name.
         ResponseEntity<UserView> responseEntity = rest.getForEntity(
-                Server.url() + RESOURCE_URL + "/accountName/" + view.getAccountName(), UserView.class);
+                Server.url() + RESOURCE_URL + "/accountName/" + view.getAccountName(), UserView.class,  headers);
 
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertTrue(responseEntity.getBody() != null);
@@ -75,7 +85,7 @@ public class UserControllerTests {
     public void testGetUserByEmail() {
         // Get user by email.
         ResponseEntity<UserView> responseEntity = rest.getForEntity(
-                Server.url() + RESOURCE_URL + "/email/" + view.getEmail(), UserView.class);
+                Server.url() + RESOURCE_URL + "/email/" + view.getEmail(), UserView.class, headers);
 
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertTrue(responseEntity.getBody() != null);
@@ -86,14 +96,14 @@ public class UserControllerTests {
         // Create user view and add user to db.
         UserView view = Mocks.createUserView();
         ResponseEntity<Long> responseEntity = rest.postForEntity(
-                Server.url() + RESOURCE_URL + "/add", new HttpEntity<>(view, Server.createHeadersJson()), Long.class);
+                Server.url() + RESOURCE_URL + "/add", new HttpEntity<>(view, Server.createHeadersJson()), Long.class, headers);
         view.setId(responseEntity.getBody());
 
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertTrue(responseEntity.getBody() != null);
 
         // Delete test data.
-        rest.delete(Server.url() + RESOURCE_URL + "/" + view.getId() + "/delete", String.class);
+        rest.delete(Server.url() + RESOURCE_URL + "/" + view.getId() + "/delete", String.class, headers);
     }
 
     @Test
@@ -101,13 +111,13 @@ public class UserControllerTests {
         // Create user view and add user to db.
         UserView view = Mocks.createUserView();
         ResponseEntity<Long> entity = rest.postForEntity(
-                Server.url() + RESOURCE_URL + "/add", new HttpEntity<>(view, Server.createHeadersJson()), Long.class);
+                Server.url() + RESOURCE_URL + "/add", new HttpEntity<>(view, Server.createHeadersJson()), Long.class, headers);
         view.setId(entity.getBody());
 
         // Delete user by id.
         ResponseEntity responseEntity = rest.exchange(
                 Server.url() + RESOURCE_URL + "/" + view.getId() + "/delete",
-                HttpMethod.DELETE, new HttpEntity<>(view, Server.createHeadersJson()), String.class);
+                HttpMethod.DELETE, new HttpEntity<>(view, Server.createHeadersJson()), String.class,headers);
 
         assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());
     }
