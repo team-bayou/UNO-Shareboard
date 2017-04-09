f84f33741ae55143224b7784dfcc2d8a6a5ddf19
Joshua <joshua.eaton@ge.com>
2017-04-09 17:52:24 -0500

    Adds permitAll() to patter for image request. needs testing

--- src/main/java/com/bayou/configurations/Security.java
+++ src/main/java/com/bayou/configurations/Security.java
@@ -12,9 +12,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class Security extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "**").permitAll().anyRequest().authenticated().and().httpBasic();
+        http.csrf().disable()   //disable csrf being needed for header in a request
+                .authorizeRequests()    //authorize the following request based on following set rules
+                .antMatchers(HttpMethod.OPTIONS, "**").permitAll() //allow any user to access this when OPTIONS
+                .antMatchers(HttpMethod.GET,"**/service/v1/images/**").permitAll() //allows GETS for given route permitted to all users
+                .anyRequest().authenticated() //catch all: this implies that if nothing matches the above two patterns, then require authentication
+                .and().httpBasic(); //utilize http basic for authentication
 
     }
 
