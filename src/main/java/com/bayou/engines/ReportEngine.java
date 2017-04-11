77625ad84618ec41901117aa76d1695d186dbdb8
Joshua <joshua.eaton@ge.com>
2017-04-10 21:09:52 -0500

    Refactors to have a single method handle message sending

--- src/main/java/com/bayou/engines/ReportEngine.java
+++ src/main/java/com/bayou/engines/ReportEngine.java
@@ -15,7 +15,7 @@ public class ReportEngine {
 
     private String reportingUserEmail;
     private String reportingUserName;
-    private String offendingUser;
+    private String offendingUserId;
     private String comments;
     private Long advertisementId;
 
@@ -29,8 +29,8 @@ public class ReportEngine {
         if(view.getAdvertisementId() != null) {
             advertisementId = view.getAdvertisementId();
         }
-        if(view.getOffendingUser() != null) {
-            offendingUser = "Reported User: " + view.getOffendingUser();
+        if(view.getOffendingUserId() != null) {
+            offendingUserId = "Reported User ID: " + view.getOffendingUserId();
         }
 
         /*Only two variables required when doing a bug report*/
@@ -50,16 +50,7 @@ public class ReportEngine {
                 new Content("text/plain", reportingUserName +"\n"+ reportingUserEmail+"'s\n" +"\n"+ comments) //set the content of the email
         );
 
-        Request request = new Request();
-        request.method = Method.POST;
-        request.endpoint = "mail/send"; //hits the endpoint on Sendgrid
-        request.body = mail.build();    //builds the request body
-
-        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY")); //gets the api key for auth from env var
-        Response response = sg.api(request); //makes the request
-        System.out.println(response.statusCode);
-        System.out.println(response.body);
-        System.out.println(response.headers);
+        send(mail);
     }
 
     public void emailAdReport(ReportView view) throws IOException {
@@ -71,11 +62,15 @@ public class ReportEngine {
                 "User Submitted Ad Report", //set the subject of the email
                 new Email("unoshareboard.dev@gmail.com"), //set who the email is to be sent to
                 new Content("text/plain",
-                        reportingUserName +"\n"+ reportingUserEmail+"'s" +"\n"+offendingUser+"\n"+"Advertisement Id: "+
+                        reportingUserName +"\n"+ reportingUserEmail+"'s" +"\n"+offendingUserId+"\n"+"Advertisement Id: "+
                                 advertisementId+"\n"+
                                 "\n" + comments) //set the content of the email
         );
 
+        send(mail);
+    }
+    /*Handles the prep and sending of mail via Sendgrid API*/
+    private void send(Mail mail) throws IOException {
         Request request = new Request();
         request.method = Method.POST;
         request.endpoint = "mail/send"; //hits the endpoint on Sendgrid
