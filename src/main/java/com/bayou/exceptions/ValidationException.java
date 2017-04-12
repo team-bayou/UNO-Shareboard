1a9f604de5f8803eb17842f35cd2704b413bb2e3
Joshua <joshua.eaton@ge.com>
2017-04-11 20:18:24 -0500

    Rebases

--- src/main/java/com/bayou/exceptions/ValidationException.java
+++ src/main/java/com/bayou/exceptions/ValidationException.java
@@ -1,9 +1,9 @@
 package com.bayou.exceptions;
 
 /**
- * Created by joshuaeaton on 4/9/17.
+ * Created by joshuaeaton on 4/11/17.
  */
-public class ValidationException extends Exception{
+public class ValidationException extends Exception {
 
     public ValidationException(String message) {
         super(message);
